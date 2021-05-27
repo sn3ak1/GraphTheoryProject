9a9cc2460220045aa8c0723e1256f45a7a0cc1a7
balabuszek <konrad.lab01@gmail.com>
2021-05-27 19:06:02 +0200

    choose nodes

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# GraphTheoryProject
+# Graph Theory Project
 
 My implementation of Ford-Fulkerson method for Maximum Flow Problem written in python.
+
+## Requirements
+Make sure you have installed all of the following prerequisites on your machine:
+* Git - [Download Git](https://git-scm.com/downloads) (OSX and Linux machines typically have this already installed.)
+* Python - [Download Python](https://www.python.org/getit/)
 
 ## Setup
 
@@ -8,16 +13,15 @@ The first thing to do is to clone the repository:
 
 ```sh
 git clone https://github.com/sn3ak1/GraphTheoryProject.git
-cd GraphTheoryProject
 ```
-Then run the algorithm by typing:
-```sh
-python main.py
+Then go to the project directory
+```
+cd GraphTheoryProject
 ```
 
 ## Usage
 
-Import graph by putting adjacency matrix in data.txt
+Import graph by putting adjacency matrix in `data.txt`
 
 Example data.txt content:
 ```
@@ -27,6 +31,13 @@ Example data.txt content:
 0, 0, 1, 0, 1,
 0, 0, 0, 0, 0,
 ```
-Remember to use source as a first node and sink as a last one.
-
 You can generate graphs in this format here: <https://graphonline.ru/en/>
+
+Run the algorithm by typing:
+```sh
+python main.py
+```
+Next script will ask to provide source node index. If you click enter without typing anything first node will be chosen as source.  
+Similarly, with the sink node but last node is default.
+
+The output will be maximum flow and paths taken. Edges in path are represented by: \[start node, end node, flow/capacity]
--- main.py
+++ main.py
@@ -32,17 +32,24 @@ class Graph:
     def __getitem__(self, item):
         return self.nodes[item]
 
+    def is_directed(self, matrix):
+        for i, _ in enumerate(matrix):
+            for j, _ in enumerate(matrix[0]):
+                if matrix[i][j] != matrix[j][i]:
+                    return True
+        return False
+
     def from_file(self, name):
         matrix = []
         with open(name, 'r') as reader:
             for line in reader.readlines():
                 matrix.append((line.strip()[:-1]).split(', '))
+        if not self.is_directed(matrix):
+            raise Exception('Graph is undirected!')
         for i, row in enumerate(matrix):
             for j, element in enumerate(row):
                 if int(element) != 0:
-                    start = 's' if i == 0 else i
-                    end = 'e' if j == len(row)-1 else j
-                    self.add_edge(start, end, int(element))
+                    self.add_edge(i, j, int(element))
 
 
 class Solver:
@@ -51,23 +58,23 @@ class Solver:
         self.max_flow = 0
         self.paths_taken = []
 
-    def shortest_path(self):
-        distance = {'s': 0}
-        path = ['e']
-        pending = [key for key in self.G['s'].successors.keys() if self.G['s'][key].remaining > 0]
+    def shortest_path(self, source, sink):
+        distance = {source: 0}
+        path = [sink]
+        pending = [key for key in self.G[source].successors.keys() if self.G[source][key].remaining > 0]
         while len(pending):
             queue = pending
             pending = []
             for key in queue:
                 distance[key] = min([distance[pred] for pred in self.G[key].predecessors.keys() if
-                                     self.G[pred][key].remaining > 0 and pred in distance.keys()]) + 1
+                                    self.G[pred][key].remaining > 0 and pred in distance.keys()]) + 1
                 keys = [x for x in self.G[key].successors.keys() if self.G[key][x].remaining > 0]
-                if 'e' in keys:
+                if sink in keys:
                     pending = []
                     break
                 pending += keys
-        best = 'e'
-        while best != 's':
+        best = sink
+        while best != source:
             predecessors = [pred for pred in self.G[best].predecessors.keys()
                             if self.G[pred][best].remaining > 0 and pred in distance.keys()]
             if not len(predecessors):
@@ -78,23 +85,41 @@ class Solver:
                     best = pred
             path.append(best)
         path.reverse()
-        return list(zip(path, path[1:]))
+        return list(map(list, zip(path, path[1:])))
 
     def flow_path(self, path):
         min_val = min(self.G[edge[0]][edge[1]].remaining for edge in path)
         self.max_flow += min_val
-        for edge in path:
+        for i, edge in enumerate(path):
             self.G[edge[0]][edge[1]].remaining -= min_val
+            path[i].append(str(self.G[edge[0]][edge[1]].capacity-self.G[edge[0]][edge[1]].remaining)
+                           + '/' + str(self.G[edge[0]][edge[1]].capacity))
 
-    def solve(self):
-        self.paths_taken.append(self.shortest_path())
+    def solve(self, source, sink):
+        if sink == -1:
+            sink = len(self.G.nodes)-1
+        if not self.G.nodes.__contains__(source) or not self.G.nodes.__contains__(sink):
+            raise Exception('Graph doesn\'t contain node with specified index.')
+
+        self.paths_taken.append(self.shortest_path(source, sink))
         while self.paths_taken[-1] is not None:
             self.flow_path(self.paths_taken[-1])
-            self.paths_taken.append(self.shortest_path())
+            self.paths_taken.append(self.shortest_path(source, sink))
         del self.paths_taken[-1]
 
 
+try:
+    source_input = input('Specify source node index (leave blank for 0): ')
+except SyntaxError:
+    source_input = ''
+try:
+    sink_input = input('Specify sink node index (leave blank for last node index): ')
+except SyntaxError:
+    sink_input = ''
+
 solver = Solver(Graph('data.txt'))
-solver.solve()
+solver.solve(int(source_input) if source_input != '' else 0, int(sink_input) if sink_input != '' else -1)
 print('Maximum flow: ' + str(solver.max_flow))
-print('Paths taken: ' + str(solver.paths_taken))
+print('Paths taken: ')
+for path in solver.paths_taken:
+    print(path)
